58c259be3f165b8427e2e65eb65ecbc12d73b3d4
Marcel-Mudrak <marcel-mudrak@wp.pl>
2022-05-10 12:46:41 +0200

    2020-05-10 MM

--- README.md
+++ README.md
@@ -2,17 +2,30 @@
 
 ## Single opinion structure on [Ceneo.pl](https://www.ceneo.pl/)
 
-|Element|Selector|Variable|
-|-------|--------|--------|
-|Opinion|div.js_product-review|opinion|
-|Opinion id|\['data-entry-id'\]|opinion_id|
-|Author|span.user-post__author-name|author|
-|Recommendation|span.user-post__author-reccomendation > em|rcmd|
-|Stars score|span.user-post__score-count|score|
-|Content|div.user-post__text|content|
-|List of adventages|div.review-feature__title--positives ~ div.review-feature__item|pros|
-|List of disadventages|div.review-feature__title--negatives ~ div.review-feature__item|cons|
-|Date of posting opinion|span.user-post_published > time:nth-child(1)'\['datetime\]|posted_on|
-|Date of purchasing product|span.user-post_published > time:nth-child(2)'\['datetime\]|bought_on|
-|For how many users useful|button.vote-yes > span|useful_for|
-|For how many users useless|button.vote-no > span|useless_for|
+|Element|Selector|Variable|Data type|
+|-------|--------|--------|---------|
+|Opinion|div.js_product-review|opinion|bs4.element.Tag|
+|Opinion id|\['data-entry-id'\]|opinion_id|str|
+|Author|span.user-post__author-name|author|str|
+|Recommendation|span.user-post__author-reccomendation > em|rcmd|bool|
+|Stars score|span.user-post__score-count|score|float|
+|Content|div.user-post__text|content|str|
+|List of adventages|div.review-feature__title--positives ~ div.review-feature__item|pros|list\[str\]|
+|List of disadventages|div.review-feature__title--negatives ~ div.review-feature__item|cons|list\[str\]|
+|Date of posting opinion|span.user-post_published > time:nth-child(1)'\['datetime\]|posted_on|str|
+|Date of purchasing product|span.user-post_published > time:nth-child(2)'\['datetime\]|bought_on|str|
+|For how many users useful|button.vote-yes > span|useful_for|int|
+|For how many users useless|button.vote-no > span|useless_for|int|
+
+# Stages of the project
+
+1) Extraction of elements for a single opinion to separate variables
+2) Extraction of elements for a single opinion 3)to one complex variable
+3) Extraction of all opinions form single page to list
+4) Extraction of all opinions for certain product and saving it to file
+5) Code refactoring annd optimazation
+    a) Definition of funnction for extracting sinngle elements of page from HTML code
+    b) Creation of dictionary that describes opinnion's structure with selectors for particular opinion's elements
+    c) Usinng dictionnary comprehension to extract all opinion's elements on the basis of opinion's structure dictionary
+6) Adjustment of data types for different opinion's elements
+7) Translation of certain opinion's elements into English
--- scraper.py
+++ scraper.py
@@ -1,57 +1,68 @@
-from encodings import utf_8
-from tkinter.messagebox import NO
-import requests
 from bs4 import BeautifulSoup
+import requests
 import json
+from translate import Translator
+
+translator= Translator(to_lang="en", from_lang="pl")
 
 def get_element(parent, selector, attribute = None, return_list = False):
     try:
         if return_list:
-            return[item.text.strip() for item in parent.select(selector)]
+            return [item.text.strip() for item in parent.select(selector)]
         if attribute:
-            return parent.select_one(selector)(attribute)
+            return parent.select_one(selector)[attribute]
         return parent.select_one(selector).text.strip()
     except (AttributeError, TypeError):
         return None
-    
-product_id = input('Please enter product id:\n')
-# 91066177
-url = f'https://www.ceneo.pl/{product_id}#tab=reviews'    
+
+opinion_elements = {
+    "author":["span.user-post__author-name"],
+    "rcmd": ["span.user-post__author-recomendation > em"],
+    "score": ["span.user-post__score-count"],
+    "content": ["div.user-post__text"],
+    "posted_on": ["span.user-post__published > time:nth-child(1)", "datetime"],
+    "bought_on": ["span.user-post__published > time:nth-child(2)", "datetime"],
+    "useful_for": ["button.vote-yes > span"],
+    "useless_for": ["button.vote-no > span"],
+    "pros": ["div.review-feature__title--positives ~ div.review-feature__item", None, True],
+    "cons": ["div.review-feature__title--negatives ~ div.review-feature__item", None, True]
+}
+
+product_id = input("Please enter the product id: ")
+url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
 
 all_opinions = []
 
-while(url):
+while (url):
+    print(url)
     response = requests.get(url)
-    page_dom = BeautifulSoup(response.text, 'html.parser')
-    opinions = page_dom.select('div.js_product-review')
+    page_dom = BeautifulSoup(response.text, "html.parser")
+    opinions = page_dom.select("div.js_product-review")
 
     for opinion in opinions:
-        
         single_opinion = {
-            'opinion_id': opinion['data-entry-id'],
-            'author': get_element(opinion, 'span.user-post__author-name'),
-            'rcmd': get_element(opinion, 'span.user-post__author-reccomendation > em'),
-            'score': get_element(opinion, 'span.user-post__score-count'),
-            'content': get_element(opinion, 'div.user-post__text'),
-            'posted_on': get_element(opinion, "span.user-post__published > time:nth-child(1)", 'datetime'),
-            'bought_on': get_element(opinion, "span.user-post__published > time:nth-child(2)", 'datetime'),
-            'useful_for': get_element(opinion, 'button.vote-yes > span'),
-            'useless_for': get_element(opinion, 'button.vote-no > span'),
-            'pros': get_element(opinion, 'div.review-feature__title--positives ~ div.review-feature__item', None, True),
-            'cons': get_element(opinion, 'div.review-feature__title--negatives ~ div.review-feature__item', None, True)
+            key: get_element(opinion, *values)
+            for key, values in opinion_elements.items() 
         }
 
+        single_opinion["opinion_id"] = opinion["data-entry-id"]
+        single_opinion['rcmd'] = True if single_opinion['rcmd'] == 'Polecam' else False if single_opinion['rcmd'] == 'Nie polecam' else None
+        single_opinion['score'] = float(single_opinion['score'].split('/')[0].replace(',', '.'))
+        single_opinion['useful_for'] = int(single_opinion['useful_for'])
+        single_opinion['useless_for'] = int(single_opinion['useless_for'])
+        single_opinion['content_en'] = translator.translate(single_opinion['content'])
+        
         all_opinions.append(single_opinion)
         
     try:
-        url = 'https://www.ceneo.pl' + page_dom.select_one('a.pagination__next')['href']
-    except TypeError:
-        print('Error')
+        url = "https://www.ceneo.pl" + get_element(page_dom,"a.pagination__next","href")
+    except TypeError: 
         url = None
-
-# print(json.dumps(all_opinions, indent=4, ensure_ascii=False))
-
+        
+        
 with open(f'opinions/{product_id}.txt', 'w', encoding='UTF-8') as file:
     for opinion in all_opinions:
         for k, v in opinion.items():
             file.write(str(k) + ':\n' + str(v) + '\n\n')
+            
+# 91066177
